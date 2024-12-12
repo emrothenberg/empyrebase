585ea57beac5e1084779a35818987ce62b792b82
emrothenberg <eitanrothenberg@duck.com>
2024-12-12 11:12:41 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='empyrebase',
-    version='1.0.0',
+    version='1.0.1',
     url='https://github.com/emrothenberg/empyrebase',
     description='A simple python wrapper for the Firebase API with current deps',
     author='emrothenberg',
